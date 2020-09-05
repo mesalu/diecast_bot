9c40f3b93c7a82c346625349d48f3724d80d6690
Mesalu <samuel@mesalu.com>
2020-09-05 06:29:03 -0700

    Small tweaks to improve quick launch script

--- launch.py
+++ launch.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 """
+Quick launch point for the bot program.
 """
 
 from bot import DieCastBot
 import keyring
 
-client = DieCastBot()
-client.run(keyring.get_password("discord", "diecast_bot"))
+if __name__ == "__main__":
+    client = DieCastBot()
+    client.run(keyring.get_password("discord", "diecast_bot"))
